101b6a72e4d66b2395b05b0be57b0d80bf5675f4
behemoth <LuisScheurenbrand@gmail.com>
2019-08-12 21:37:39 +0200

    removed threading tests

--- source/main.cpp
+++ source/main.cpp
@@ -10,34 +10,7 @@
 #define DEBUG 1
 using namespace swurl;
 model::theme theme;
-static bool comic;
-static model::comic newComic;
 
-static void dlComics(void* args) {
-    WebRequest * request = new WebRequest(web::FORMAT_ALL + "1");
-    SessionManager::makeRequest(request);
-    Document d;
-    d.Parse(request->response.rawResponseBody.c_str());
-    std::vector<model::comic> comics = web::getComics(d["result"]);
-    /*for(model::comic comic : comics) {
-        web::getPath(comic, 1, false);
-        *newComic = comic;
-    } */
-    fs::mkpath("comics/" + comics[0].id + "/");
-    web::getPath(comics[0], 1, false);
-    newComic = comics[0];
-    comic = true;
-    
-    /*const Value& comicsJson = d["result"];
-    if(comicsJson.IsArray());
-    for(const Value& comicJson : comicsJson.GetArray()) {
-        model::comic comic = web::getComic(comicJson);
-        web::getPath(comic, 1, false);
-        *newComic = comic;
-        //comics.push_back(comic);
-        //newComic = true;
-    }*/
-}
 void init(){
     printf("setting theme");
     theme = *new model::theme;
@@ -63,42 +36,14 @@ int main(int argc, char **argv) {
         nxlinkStdio();
     #endif
 
-    /*init();
+    init();
 
     printf("starting application...\n");
     ui::MainApplication *amain = new ui::MainApplication();
     amain->Show();
     printf("cleaning up...\n");
-    delete amain; */
+    delete amain;
 
-    //newComic = nullptr;
-    comic = false;
-    printf("creating thread...\n");
-    Thread dlThread;
-    threadCreate(&dlThread, dlComics, nullptr, 0x2000, 0x2C, -2);
-    threadStart(&dlThread);
-
-    while(appletMainLoop()) {
-        hidScanInput();
-        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-
-        if (kDown & KEY_PLUS)
-            break;
-        
-        if(comic) {
-            printf("\n\nnew Comic\n\n");
-            //for(model::comic comic : comics) {
-                printf("comic: %s, %s, %s\n",
-                        newComic.name.c_str(),
-                        newComic.id.c_str(),
-                        newComic.mediaId.c_str());
-            //}
-            //newComic = nullptr;
-        }
-
-    }
-    threadWaitForExit(&dlThread);
-    threadClose(&dlThread);
     SessionManager::dealloc();
     printf("exiting...\n");
     return 0;
